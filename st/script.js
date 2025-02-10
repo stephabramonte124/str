c24cfe8d0ca66a9412c3c3524417593c71ff4643
stephabramonte124 <stephanoabramonte@gmail.com>
2025-02-09 23:50:09 -0500

    Add files via upload

---
+++ st/script.js
@@ -0,0 +1,13 @@
+const btnNo = document.querySelector("#btn-random")
+
+function moverAleatoriamente(btn) {
+   btn.style.fontWeight = "bolder";
+   btn.style.position = "absolute";
+   btn.style.top = Math.floor(Math.random() * 90 + 5) + "%"
+   btn.style.left = Math.floor(Math.random() * 90 + 5) + "%"
+
+}
+
+btnNo.addEventListener("mouseenter", function(e){
+    moverAleatoriamente(e.target)
+})
